31c1a44c62e5a84b4bb6589b4cf520eb314d6831
Laurynas Alekna <laurynas@alekna.org>
2021-01-23 16:11:22 +0000

    Extend TradesNotification type

--- models/trades_notification.go
+++ models/trades_notification.go
@@ -1,3 +1,32 @@
 package models
 
 type TradesNotification []Trade
+
+func (tn TradesNotification) Count() int {
+	return len(tn)
+}
+
+func (tn TradesNotification) VWAP() float64 {
+	var size, sizeCurrency float64
+	for _, t := range tn {
+		size += t.Amount
+		sizeCurrency += t.Amount / t.Price
+	}
+	return size / sizeCurrency
+}
+
+func (tn TradesNotification) FirstTrade() Trade {
+	return tn[0]
+}
+
+func (tn TradesNotification) LastTrade() Trade {
+	return tn[len(tn)-1]
+}
+
+func (tn TradesNotification) Direction() string {
+	return tn.FirstTrade().Direction
+}
+
+func (tn TradesNotification) Gap() float64 {
+	return tn.LastTrade().Price/tn.FirstTrade().Price - 1
+}
